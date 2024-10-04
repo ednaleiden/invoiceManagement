0643be6c4dfcc1429a830f04f258b50da8b98be1
ednaleiden <ednadupont@hotmail.com>
2024-10-03 21:36:06 -0500

    create project

---
+++ src/main/java/com/api/gestion/GestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gestion/GestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
